532bf3b9995552b23b9c0ce33724b12f77e8675d
jl486 <jonathanliu2009@gmail.com>
2025-03-03 22:33:29 -0500

    feat: flipping animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { GameProvider } from "./contexts/GameContext";
+
 import Game from "./components/Game";
+import { GameProvider } from "./contexts/GameContext";
 import { Solution } from "./types";
 import "./App.css";
 
@@ -23,13 +24,20 @@ export default function App() {
   return (
     <GameProvider>
       <header
-        className="w-full flex flex-row justify-between items-center
-          h-14 bg-background-white dark:bg-background-dark
+        className="
+          w-full flex flex-row justify-between items-center h-14
+          bg-background-white dark:bg-background-dark
           border-b-[1px] border-border-light dark:border-border-dark
-          tracking-wider font-bold text-3xl
         "
       >
-        WORDLE
+        <button className="p-3 mx-1">
+          <svg className="fill-black dark:fill-white" width="32" height="32" viewBox="0 0 8.467 8.467">
+            <rect width="5.444" height=".646" x="1.511" y="5.383" ry=".323" />
+            <rect width="5.444" height=".646" x="1.511" y="3.91" ry=".323"/>
+            <rect width="5.444" height=".646" x="1.511" y="2.437" ry=".323"/>
+          </svg>
+        </button>
+        <h1 className="tracking-wider font-bold text-3xl">WORDLE</h1>
       </header>
       <Game solution={solution?.word} />
     </GameProvider>
--- frontend/src/components/Row.tsx
+++ frontend/src/components/Row.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Letter } from "../types";
 import { RowItem } from "./RowItem";
 
@@ -7,13 +8,34 @@ interface RowProps {
 }
 
 export default function Row({ guess, currentGuess }: RowProps) {
+  const [flipped, setIsFlipped] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (guess) {
+      guess.forEach((_, i) => {
+        setTimeout(() => {
+          setIsFlipped((prev) => [...prev, i]);
+        }, i * 300 + 300)
+      })
+    }
+  }, [guess])
+
+  const animationDelays = [
+    "[animation-delay:0.0s]",
+    "[animation-delay:0.3s]",
+    "[animation-delay:0.6s]",
+    "[animation-delay:0.9s]",
+    "[animation-delay:1.2s]"
+  ];
+
   if (guess) {
     return (
       <div className="text-center flex justify-center">
         {guess.map((item, i) => (
           <RowItem
             key={i}
-            variant={item.color}
+            variant={flipped.includes(i) ? item.color : "current"}
+            className={`animate-flip ${animationDelays[i]}`}
           >
             {item.key}
           </RowItem>
